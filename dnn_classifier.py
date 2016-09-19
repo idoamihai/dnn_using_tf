1f1a0eb70927948a98626b8a4d02657bacc2b747
Ido Amihai <idoamihai@gmail.com>
2016-09-19 17:23:07 +0300

    little debugging

--- dnn_classifier.py
+++ dnn_classifier.py
@@ -6,30 +6,11 @@ Created on Wed Sep 14 16:32:35 2016
 """
 
 import tensorflow as tf
-from tensorflow.python.ops import rnn, rnn_cell
 import numpy as np
-import pickle, os, random
 from sklearn import cross_validation, preprocessing, metrics, grid_search
-import tensorflow.contrib.learn as skflow
+import random
 
-
-os.chdir('/Users/idoamihai/Documents/Bandmanage/falling/fall_detection')
-
-x_train,y_train,x_test,y_test = pickle.load(open( "data_lstm.p", "rb" ))
-
-params = {'num_steps':1001,'hidden':[10,10],'l2_regul':1e-2,'keep_prob':0.5,
-        'n_folds': 5}
-    
-       
-features = x_train.columns
-       
-x_train = x_train[features] 
-x_test = x_test[features]
-
-example_rows = 1
-example_columns = x_train.shape[1]
-num_labels=2
-def reformat(dataset, labels):
+def reformat(dataset, labels, num_labels, example_rows, example_columns):
   dataset = dataset.reshape((-1, example_rows * example_columns)).astype(np.float32)
   # Map 2 to [0.0, 1.0, 0.0 ...], 3 to [0.0, 0.0, 1.0 ...]
   labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
@@ -38,6 +19,23 @@ def reformat(dataset, labels):
 def accuracy(predictions, labels):
   return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
           / predictions.shape[0])
+  
+def grid_search_dnn(param_grid,x_train,y_train,example_rows=1,num_labels=2,n_folds=5):
+    max_score = 0
+    max_params = {}
+    grid = grid_search.ParameterGrid(param_grid)
+    grid_search_scores = []
+    for params_ in grid:
+        clf = dnn(params_)
+        score = clf.cv_score(x_train,y_train,example_rows,num_labels,n_folds)
+        if score > max_score:
+            max_score = score
+            max_params = params_
+        dict_ = params_
+        dict_['score'] = score
+        grid_search_scores.append(dict_) 
+    return grid_search_scores,max_params,max_score
+
 
 class dnn():
     
@@ -60,7 +58,7 @@ class dnn():
         keep_prob = self.params['keep_prob'] #give 1 for no dropout
         if 'random_seed' in self.params:
             kf = cross_validation.StratifiedKFold(y=y_train,
-                              n_folds=n_folds,shuffle=True,random_state = params['random_seed'])
+                              n_folds=n_folds,shuffle=True,random_state = self.params['random_seed'])
         else:
             kf = cross_validation.StratifiedKFold(y=y_train,n_folds=n_folds,shuffle=True)  
 
@@ -73,9 +71,9 @@ class dnn():
             x_valid = x_train.iloc[valid_idx]
             y_valid = y_train.iloc[valid_idx]
             
-            train_dataset, train_labels = reformat(np.array(x_train_), np.array(y_train_))
-            valid_dataset, valid_labels = reformat(np.array(x_valid), np.array(y_valid))
-            #test_dataset, test_labels = reformat(np.array(x_test), np.array(y_test))
+            train_dataset, train_labels = reformat(np.array(x_train_), np.array(y_train_),num_labels,example_rows,example_columns)
+            valid_dataset, valid_labels = reformat(np.array(x_valid), np.array(y_valid),num_labels,example_rows,example_columns)
+            #test_dataset, test_labels = reformat(np.array(x_test), np.array(y_test),num_labels,example_rows,example_columns)
             
             if 'scale' in self.params:
                 scaler = preprocessing.StandardScaler()
@@ -115,10 +113,10 @@ class dnn():
               layer = {}
               dropout = {}
               layer[0] = tf.nn.relu(tf.matmul(tf_train_dataset,weights[0]) + biases[0])
-              dropout[0] = tf.nn.dropout(layer[0], keep_prob=keep_prob, seed=0)
+              dropout[0] = tf.nn.dropout(layer[0], keep_prob=keep_prob)
               for i in np.arange(1,len(weights)-1):
                   layer[i] = tf.nn.relu(tf.matmul(dropout[i-1],weights[i]) + biases[i])
-                  dropout[i] = tf.nn.dropout(layer[i], keep_prob=keep_prob, seed=0)
+                  dropout[i] = tf.nn.dropout(layer[i], keep_prob=keep_prob)
               logits = tf.matmul(dropout[len(dropout)-1],weights[len(weights)-1]) + biases[len(biases)-1]     
                   
               loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels)) 
@@ -208,11 +206,11 @@ class dnn():
         hidden = self.params['hidden']
         l2_regul = self.params['l2_regul']
         keep_prob = self.params['keep_prob']
-        train_dataset, train_labels = reformat(np.array(x_train), np.array(y_train))
+        train_dataset, train_labels = reformat(np.array(x_train), np.array(y_train),num_labels,example_rows,example_columns)
         if 'x_valid' and 'y_valid' in kwargs:
             x_valid, y_valid = kwargs['x_valid'],kwargs['y_valid']
-            valid_dataset, valid_labels = reformat(np.array(x_valid), np.array(y_valid))
-        test_dataset, test_labels = reformat(np.array(x_test), np.array(y_test))
+            valid_dataset, valid_labels = reformat(np.array(x_valid), np.array(y_valid),num_labels,example_rows,example_columns)
+        test_dataset, test_labels = reformat(np.array(x_test), np.array(y_test),num_labels,example_rows,example_columns)
         if 'scale' in self.params:
             scaler = preprocessing.StandardScaler()
             scaler.fit(train_dataset)        
@@ -255,10 +253,10 @@ class dnn():
           layer = {}
           dropout = {}
           layer[0] = tf.nn.relu(tf.matmul(tf_train_dataset,weights[0]) + biases[0])
-          dropout[0] = tf.nn.dropout(layer[0], keep_prob=keep_prob, seed=0)
+          dropout[0] = tf.nn.dropout(layer[0], keep_prob=keep_prob)
           for i in np.arange(1,len(weights)-1):
               layer[i] = tf.nn.relu(tf.matmul(dropout[i-1],weights[i]) + biases[i])
-              dropout[i] = tf.nn.dropout(layer[i], keep_prob=keep_prob, seed=0)
+              dropout[i] = tf.nn.dropout(layer[i], keep_prob=keep_prob)
           logits = tf.matmul(dropout[len(dropout)-1],weights[len(weights)-1]) + biases[len(biases)-1]     
               
           loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels)) 
@@ -345,7 +343,7 @@ class dnn():
           #test_pred = np.argmax(test_pred_proba,axis=1)
         return score
         
-    def fit_predict(self,x_train,y_train,example_rows,num_labels,is_train,**kwargs):
+    def fit_predict(self,x,y,example_rows,num_labels,is_train,**kwargs):
         #refit and predict on test
         if 'patience' in self.params:
             patience = self.params['patience']
@@ -353,16 +351,16 @@ class dnn():
             improvement_threshold = 0.995 # a relative improvement of this much is considered significant
         if 'random_seed' in self.params:
             random.seed(self.params['random_seed'])
-        example_columns = x_train.shape[1]
+        example_columns = x.shape[1]
         num_steps = self.params['num_steps']
         hidden = self.params['hidden']
         l2_regul = self.params['l2_regul']
         keep_prob = self.params['keep_prob']
-        train_dataset, train_labels = reformat(np.array(x_train), np.array(y_train))
+        train_dataset, train_labels = reformat(np.array(x), np.array(y),num_labels,example_rows,example_columns)
         if 'x_valid' and 'y_valid' in kwargs:
             x_valid, y_valid = kwargs['x_valid'],kwargs['y_valid']
-            valid_dataset, valid_labels = reformat(np.array(x_valid), np.array(y_valid))
-        test_dataset, test_labels = reformat(np.array(x_test), np.array(y_test))
+            valid_dataset, valid_labels = reformat(np.array(x_valid), np.array(y_valid),num_labels,example_rows,example_columns)
+        test_dataset, test_labels = reformat(np.array(x), np.array(y),num_labels,example_rows,example_columns)
         if 'scale' in self.params:
             scaler = preprocessing.StandardScaler()
             scaler.fit(train_dataset)        
@@ -405,10 +403,10 @@ class dnn():
           layer = {}
           dropout = {}
           layer[0] = tf.nn.relu(tf.matmul(tf_train_dataset,weights[0]) + biases[0])
-          dropout[0] = tf.nn.dropout(layer[0], keep_prob=keep_prob, seed=0)
+          dropout[0] = tf.nn.dropout(layer[0], keep_prob=keep_prob)
           for i in np.arange(1,len(weights)-1):
               layer[i] = tf.nn.relu(tf.matmul(dropout[i-1],weights[i]) + biases[i])
-              dropout[i] = tf.nn.dropout(layer[i], keep_prob=keep_prob, seed=0)
+              dropout[i] = tf.nn.dropout(layer[i], keep_prob=keep_prob)
           logits = tf.matmul(dropout[len(dropout)-1],weights[len(weights)-1]) + biases[len(biases)-1]     
               
           loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels)) 
@@ -493,29 +491,9 @@ class dnn():
                 return test_pred
   
         
-def grid_search_dnn(param_grid):
-    max_score = 0
-    max_params = {}
-    grid = grid_search.ParameterGrid(param_grid)
-    grid_search_scores = []
-    for params_ in grid:
-        clf = dnn(params_)
-        score = clf.cv_score(x_train,y_train,example_rows=1,num_labels=2,n_folds=5)
-        if score > max_score:
-            max_score = score
-            max_params = params_
-        dict_ = params_
-        dict_['score'] = score
-        grid_search_scores.append(dict_) 
-    return grid_search_scores,max_params,max_score
 
         
         
-'''       
-param_grid = {'keep_prob': [0.4,0.5, 0.6], 
-'hidden': [[10],[50],[50,10]],
-'l2_regul':[1e-2,1e-3],'num_steps':[51],'scale':[True]}
-'''                                   
 
         
         
