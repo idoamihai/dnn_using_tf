9b4e0c564312397f0f0d9e4a91a91cc41bcb0025
Ido Amihai <idoamihai@gmail.com>
2016-09-28 02:26:24 +0300

    lstm classifier

--- dnn_classifier.py
+++ dnn_classifier.py
@@ -9,6 +9,8 @@ import tensorflow as tf
 import numpy as np, pandas as pd
 from sklearn import cross_validation, preprocessing, metrics, grid_search
 import random
+from tensorflow.python.ops import rnn, rnn_cell
+
 
 def reformat(dataset, labels, num_labels, example_rows, example_columns):
   dataset = dataset.reshape((-1, example_rows * example_columns)).astype(np.float32)
@@ -35,7 +37,24 @@ def grid_search_dnn(param_grid,x_train,y_train,example_rows=1,num_labels=2,n_fol
         dict_['score'] = score
         grid_search_scores.append(dict_) 
     return grid_search_scores,max_params,max_score  
+    
 
+def grid_search_lstm(param_grid,x_train,y_train,num_labels=2,n_input=4,n_timesteps=31,n_folds=5,scoring=metrics.accuracy_score):
+    max_score = 0
+    max_params = {}
+    grid = grid_search.ParameterGrid(param_grid)
+    grid_search_scores = []
+    for params_ in grid:
+        clf = lstm(params_)
+        score = clf.cv_score(x_train,y_train,n_input,n_timesteps,num_labels,n_folds,scoring=scoring,predict_proba=False)
+        if score > max_score:
+            max_score = score
+            max_params = params_
+        dict_ = params_
+        dict_['score'] = score
+        grid_search_scores.append(dict_) 
+    return grid_search_scores,max_params,max_score     
+    
 class dnn():
     
     def __init__(self,params):
@@ -212,6 +231,7 @@ class dnn():
           saver = tf.train.Saver()
           print('Initialized')
           best_validation_loss = np.inf
+          best_training_loss = np.inf
           patience_steps = 0
           tracking = []
           for step in range(num_steps):
@@ -249,25 +269,28 @@ class dnn():
                       print('Validation Loss at step %d: %f' % (step, vl))
                       print('Validation accuracy: %.1f%%' % accuracy(
                                  vp, valid_labels))
-                  #print('Validation accuracy: %.1f%%' % accuracy(
-                  if 'patience' in self.params and ('x_valid' and 'y_valid' in kwargs):
-                      if vl < best_validation_loss:
-                          saver.save(session, 'model_best.ckpt')
-                          if vl < best_validation_loss*improvement_threshold:
-                              patience_steps = 0
+                  if 'x_valid' and 'y_valid' in kwargs:
+                      if 'patience' in self.params:
+                          if vl < best_validation_loss:
+                              saver.save(session, 'model_best.ckpt')
+                              if vl < best_validation_loss*improvement_threshold:
+                                  patience_steps = 0
+                              else:
+                                  patience_steps += 1 #small improvement
                               best_validation_loss = vl
                           else:
-                              patience_steps += 1 #small improvement
-                              best_validation_loss = vl
-                      else:
-                          patience_steps += 1
-                          if ((patience_steps >= patience_increase) and (step >= patience)):
-                              if 'x_test' and 'y_test' in kwargs:
-                                  saver.restore(session, 'model_best.ckpt')
-                                  print('Test accuracy: %.1f%%' % accuracy(
-                                           test_prediction.eval(),test_labels))
-                              break  
-                  else:
+                              patience_steps += 1
+                              if ((patience_steps >= patience_increase) and (step >= patience)):
+                                  if 'x_test' and 'y_test' in kwargs:
+                                      saver.restore(session, 'model_best.ckpt')
+                                      print('Test accuracy: %.1f%%' % accuracy(
+                                               test_prediction.eval(),test_labels))
+                                  break  
+                  elif vl < best_validation_loss:
+                      best_validation_loss = vl
+                      saver.save(session, 'model_best.ckpt')  
+                  elif l < best_training_loss:
+                      best_training_loss = l
                       saver.save(session, 'model_best.ckpt')  
                 elif step == num_steps-1:
                   if 'x_test' and 'y_test' in kwargs:
@@ -284,7 +307,198 @@ class dnn():
         if is_train:
             return tracking
   
-        
+class lstm():        
+    def __init__(self,params):
+        self.params = params
+
+    def fit_predict(self,train_data,train_labels,n_input,n_timesteps,n_classes,is_train,predict_proba=False,**kwargs):
+        #refit and predict on test
+        graph = tf.Graph()
+        with graph.as_default():
+            # Parameters
+            if self.params['learning_rate'] == 'exp_decay':
+                global_step = tf.Variable(0)
+                learning_rate = tf.train.exponential_decay(
+               0.5, global_step, 1000, 0.5, staircase=True) 
+            else:                
+                learning_rate = self.params['learning_rate']
+            training_iters = self.params['training_iters']
+            if 'patience' in self.params:
+                patience = self.params['patience']
+                patience_increase = 5 #wait this much longer when a new best is found
+                improvement_threshold = 0.995 # a relative improvement of this much is considered significant
+            if 'random_seed' in self.params:
+                random.seed(self.params['random_seed'])
+            if 'batch_size' in self.params:
+                batch_size = self.params['batch_size']
+            else:
+                batch_size = len(train_data) #batch optimizer
+            if 'n_layers' in self.params:
+                n_layers = self.params['n_layers']
+            else:
+                n_layers = 1
+            display_step = self.params['display_step']            
+            # Network Parameters
+            n_hidden = self.params['n_hidden'] # hidden layer num of features
+            if is_train == False:
+                keep_prob = 1.0 #no dropout
+            else:
+                if 'keep_prob' in self.params:
+                    keep_prob = self.params['keep_prob']
+                else:
+                    keep_prob = 1.0
+            if 'l2_regul' in self.params:
+                beta_regul = self.params['l2_regul']
+            else:
+                beta_regul = 0
+            n_classes = n_classes             
+            # tf Graph input
+            x = tf.placeholder("float", [None, n_timesteps, n_input])
+            y = tf.placeholder("float", [None, n_classes])            
+            # Define weights
+            weights = {
+                'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+            }
+            biases = {
+                'out': tf.Variable(tf.random_normal([n_classes]))
+            }                       
+            def RNN(x, weights, biases):            
+                # Prepare data shape to match `rnn` function requirements
+                # Current data input shape: (batch_size, n_steps, n_input)
+                # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)            
+                # Permuting batch_size and n_steps
+                x = tf.transpose(x, [1, 0, 2])
+                # Reshaping to (n_steps*batch_size, n_input)
+                x = tf.reshape(x, [-1, n_input])
+                # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+                x = tf.split(0, n_timesteps, x)            
+                # Define a lstm cell with tensorflow
+                lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0, state_is_tuple=True)            
+                stacked_lstm = rnn_cell.MultiRNNCell([lstm_cell] * n_layers,
+                                                     state_is_tuple=True)
+                # Get lstm cell output
+                outputs, states = rnn.rnn(stacked_lstm, x, dtype=tf.float32)            
+                # Linear activation, using rnn inner loop last output
+                drop = tf.nn.dropout(outputs[-1], keep_prob=keep_prob)
+                pred = tf.matmul(drop, weights['out']) + biases['out'] 
+                return pred           
+            pred = RNN(x, weights, biases)  
+            probabilities = tf.nn.sigmoid(pred)
+            # Define loss and optimizer
+            cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(pred, y)) +\
+            beta_regul * tf.nn.l2_loss(weights['out'])
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)            
+            # Evaluate model
+            correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+            accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))            
+            # Initializing the variables
+            init = tf.initialize_all_variables()        
+        # Launch the graph
+        with tf.Session(graph=graph) as sess:
+            sess.run(init)
+            saver = tf.train.Saver()
+            step = 1
+            # Keep training until reach max iterations
+            if is_train:
+                best_validation_loss = np.inf
+                best_training_loss = np.inf
+                patience_steps = 0
+                tracking = []
+                for step in range(training_iters):
+                    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+                    batch_x = train_data[offset:(offset + batch_size), :]
+                    batch_y = train_labels[offset:(offset + batch_size), :]
+                    batch_x = batch_x.reshape((batch_size, n_timesteps, n_input))
+                    # Run optimization op (backprop)
+                    sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+                    if step % display_step == 0:
+                        # Calculate batch accuracy
+                        acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+                        # Calculate batch loss
+                        loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+                        print("Iter " + str(step) + ", Minibatch Loss= " + \
+                              "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                              "{:.5f}".format(acc))
+                        if 'x_valid' and 'y_valid' in kwargs:
+                            x_valid = kwargs['x_valid'].reshape((-1,n_timesteps,n_input))
+                            vl = sess.run(cost,feed_dict={x:x_valid,y:kwargs['y_valid']})
+                            print ("validation loss %.6f" %vl)
+                            print ("validation accuracy:", \
+                                    sess.run(accuracy, feed_dict={x:x_valid,y:kwargs['y_valid']}))
+                            tracking.append([step,loss,vl])
+                            if 'patience' in self.params:
+                                if vl < best_validation_loss:
+                                    saver.save(sess, 'lstm_best.ckpt')
+                                    if vl < best_validation_loss*improvement_threshold:
+                                        patience_steps = 0
+                                    else:
+                                        patience_steps += 1 #small improvement
+                                    best_validation_loss = vl
+                                else:
+                                    patience_steps += 1
+                                    if ((patience_steps >= patience_increase) and (step >= patience)):
+                                        if 'x_test' and 'y_test' in kwargs:
+                                            saver.restore(sess, 'lstm_best.ckpt')
+                                            test_data = kwargs['test_data'].reshape((-1,n_timesteps,n_input))
+                                            print("Testing Accuracy:", \
+                                                  sess.run(accuracy, feed_dict={x: test_data, y: kwargs['test_labels']}))
+                                        break  
+                            elif vl < best_validation_loss:
+                                saver.save(sess, 'lstm_best.ckpt') 
+                        else:
+                            tracking.append([step,loss])
+                            if loss < best_training_loss:
+                                saver.save(sess, 'lstm_best.ckpt')                     
+                    elif step == training_iters-1:
+                        if 'x_test' and 'y_test' in kwargs:
+                            test_data = kwargs['test_data'].reshape((-1,n_timesteps,n_input))
+                            print("Testing Accuracy:", \
+                              sess.run(accuracy, feed_dict={x: test_data, y: kwargs['test_labels']}))
+                print("Optimization Finished!")            
+                # Calculate accuracy on the validation set
+                if (('test_data' in kwargs) and ('test_labels' in kwargs)):
+                    test_data = kwargs['test_data'].reshape((-1,n_timesteps,n_input))
+                    print("Testing Accuracy:", \
+                    sess.run(accuracy, feed_dict={x: test_data, y: kwargs['test_labels']}))
+                    pred_proba = sess.run(probabilities, feed_dict={x: test_data, y: kwargs['test_labels']})
+                    pred_class = np.argmax(pred_proba,axis=1)
+            else:
+                saver.restore(sess, 'lstm_best.ckpt')
+                train_data = train_data.reshape((-1,n_timesteps,n_input))
+                pred_proba = sess.run(probabilities, feed_dict={x: train_data, y: train_labels})
+                #since we're only predicting, the inputs are train_data and train_labels
+                pred_class = np.argmax(pred_proba,axis=1) 
+                if predict_proba==True:
+                    return pred_proba
+                else:
+                    return pred_class
+            if is_train:
+                return tracking
+                    
+    def cv_score(self,x_train,y_train,n_input,n_timesteps,n_classes,n_folds,**kwargs):
+        #normally you wouldn't use early-stopping here but when you have separate training/validation/test sets
+        #get parameter values
+        if 'scoring' in kwargs:
+            scoring = kwargs['scoring']
+        else:
+            scoring = metrics.accuracy_score
+        if 'random_seed' in self.params:
+            kf = cross_validation.StratifiedKFold(y=np.argmax(y_train,axis=1),
+              n_folds=n_folds,shuffle=True,random_state = self.params['random_seed'])
+        else:
+            kf = cross_validation.StratifiedKFold(y=np.argmax(y_train,axis=1),n_folds=n_folds,shuffle=True)  
+        kscores = []
+        fold = 0
+        for train_idx, valid_idx in kf:
+            fold += 1
+            x_train_ = x_train[train_idx]
+            y_train_ = y_train[train_idx]
+            x_valid = x_train[valid_idx]
+            y_valid = y_train[valid_idx]
+            lstm.fit_predict(self,x_train_,y_train_,n_input,n_timesteps,n_classes,is_train=True,predict_proba=False)
+            valid_prediction = lstm.fit_predict(self,x_valid,y_valid,n_input,n_timesteps,n_classes,is_train=False,predict_proba=False)
+            kscores.append(scoring(np.argmax(y_valid,axis=1),valid_prediction))
+        return np.mean(kscores) 
 
         
         
